ca1d6d28575b67a83a1d92496f142f40b374a4e3
sumitrwk90 <sumitrwk90@gmail.com>
2026-04-06 17:02:23 +0530

    finally added requirements and setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The router engine have the capability of multi-loop agentic decision making',
+    author='sumit-kumar',
+    license='',
+)
